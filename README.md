3287ec2864542d25d3fde9495da5c7567e705fe8
Jake Cannon <jakecan13@gmail.com>
2019-03-30 10:38:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Trash Man bot 
+
+![trashman_desize_2](/Users/JakeCannon/Desktop/trashman_desize_2.png)
+
+Using the Twitter api and the Google Maps api this bot finds the three closest public waste baskets to help reduce litter.
+
+
+
+## Usage
+
+
+
+
+
+## Built With
+
+- Python
+- Twitter api
+- Google Maps api
---
+++ bot.py
@@ -0,0 +1,22 @@
+import tweepy
+
+CONSUMER_KEY = 'X'
+CONSUMER_SECRET = 'X'
+ACCESS_KEY = 'X'
+ACCESS_SECRET = 'X'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+#this 'api object' is basically what talks to Twitter, read data, write data
+api = tweepy.API(auth)
+
+print("This is my bot")
+
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+	# print(str(mention.id) + ' - ' +  mention.text)
+	if '#nearme' in mention.text.lower():
+		print('found our keyword!' + mention.text)
