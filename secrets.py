ab0e5f17d677a15d432bbffcb179360f3431b38a
Jake Cannon <jakecan13@gmail.com>
2019-08-11 12:44:58 -0400

    fix missing line in secrets.py

--- secrets.py
+++ secrets.py
@@ -3,3 +3,4 @@ from os import environ
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
